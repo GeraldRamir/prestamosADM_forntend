d536bde28db9fb53855f15b573a82bfac5cc61c2
GeraldRamir <lucianoramirezgerald@gmail.com>
2025-04-27 18:52:03 -0400

    Cacheando todo final

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         enabled: false, // Habilitar PWA también en desarrollo
       },
       workbox: {
-        cacheId: 'prestamos-app-v3',
+        cacheId: 'prestamos-app-v8',
         globPatterns: [
           '**/*.{js,css,html,png,webmanifest,jsx,ico,svg}', // Añadido .ico y .svg
         ],
