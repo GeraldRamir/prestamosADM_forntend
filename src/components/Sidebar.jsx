69f0c9fbd11462e28aff6c70c8d08cc8cedcaa54
GeraldRamir <lucianoramirezgerald@gmail.com>
2025-04-30 02:38:45 -0400

    ads

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -447,7 +447,7 @@ const handleInstallClick = async () => {
           className="btn btn-success"
           onClick={handleInstallClick}
         >
-          aplicacion
+          instalaya
         </button>
       )}
     </div>
